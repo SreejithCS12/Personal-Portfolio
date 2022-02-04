7ba68d6996cedbfb8318b38cc769e3e196f33073
Sreejith C S <88240382+SreejithCS12@users.noreply.github.com>
2022-02-04 18:51:36 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,35 @@
+// $(document).ready(function() {
+
+//     $('menu').click(function() {
+//         $(this).toggleClass('fa-times');
+//         $('header').toggleClass('toggle');
+
+
+//     });
+
+//     $(window).on('scroll load', function() {
+
+//         $('#menu').removeClass('fa-times');
+//         $('header').removeClass('toggle');
+
+
+//     });
+
+// });
+
+$(document).ready(function() {
+
+    $('#menu').click(function() {
+        $(this).toggleClass('fa-times');
+        $('header').toggleClass('toggle');
+    });
+
+    $(window).on('scroll load', function() {
+        $('#menu').removeClass('fa-times');
+        $('header').removeClass('toggle');
+    });
+
+
+
+
+});
